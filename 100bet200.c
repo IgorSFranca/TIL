f55b03c865e2b08a288fab764bd020034f568984
Igor França ∴ <125696858+IgorSFranca@users.noreply.github.com>
2023-06-23 09:42:54 -0400

    100bet200

---
+++ 100bet200.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    int num, cont = 0, asw = 1;
+    printf("---------------------------------\n");
+    printf("         100 between 200         \n");
+    printf("---------------------------------\n\n");
+    do {
+    printf("Informe um numero inteiro: ");
+    scanf("%i", &num);
+    if (num >= 100 && num <= 200)
+        cont ++;
+    printf ("Deseja continuar inserindo numeros? [0 = Nao]: ");
+    scanf("%i", &asw);
+    } while (asw != 0);
+    if (cont == 1)
+        printf("\nFoi inserido %i numero entre 100 e 200.\n\n", cont);
+    else if (cont == 0)
+        printf("\nNao foram inseridos numeros entre 100 e 200.\n\n");
+    else
+        printf("\nForam inseridos %i numeros entre 100 e 200.\n\n", cont);
+    system ("pause");
+    return 0;
+}
