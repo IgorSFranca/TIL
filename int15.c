6a6b49ebc6fd2252c765ef3bb9240f80fce1c016
Igor França ∴ <125696858+IgorSFranca@users.noreply.github.com>
2023-06-29 14:13:31 -0400

    Int15

---
+++ int15.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    int i;
+    printf("Numeros inteiros de 3 a 15, menos o 11\n");
+    for (i = 3; i <= 15; i++)
+        printf("%i\n", i);
+
+    system ("pause");
+    return 0;
+}
