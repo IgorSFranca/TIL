4445883e9ac44aaa4513d605dbc5cc6fbe72ecf4
Igor França <125696858+IgorSFranca@users.noreply.github.com>
2023-07-02 18:50:41 -0400

    V1
    Escreva um programa que declare um array de caracteres contendo uma palavra. Em seguida, verifique se a palavra é um palíndromo (ou seja, se ela é a mesma lida de trás para frente) e imprima uma mensagem correspondente.

---
+++ palindromo.c
@@ -0,0 +1,30 @@
+/*Escreva um programa que declare um array de caracteres contendo uma palavra. 
+Em seguida, verifique se a palavra é um palíndromo (ou seja, se ela é a mesma lida de 
+trás para frente) e imprima uma mensagem correspondente.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (){
+  int i, cont = 0;
+  char palindo[] = {"omo"}, pavindo[sizeof(palindo)-2];
+  printf("A palavra atual eh: %s\n", palindo); //apresentando a palavra
+  for (i=0; i<= (sizeof(palindo)-2); i++) //invertendo as letras no outro array
+    pavindo[(sizeof(palindo)-2-i)] = palindo[i]; 
+  printf("A palavra invertida eh: ");
+  for (i=0; i<=(sizeof(palindo)-2); i++) //mostrando o array com as letras invertidas
+    printf("%c", pavindo[i]); 
+  printf("\n");
+  for (i=0; i<=(sizeof(palindo)-2); i++){//calculando se as letras sao iguais
+    if (palindo[i] == pavindo[i])
+      cont++;
+  }
+  if (cont == (sizeof(palindo)-1))//mostrando o resultado encontrado
+    printf ("A palavra EH UM palindromo.\n");
+  else
+    printf("A palavra NAO EH um palindromo.\n");
+  printf("\n");
+  system ("pause");
+  return 0;
+}
