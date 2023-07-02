23dc433a7b860be75d6439181c78fca169e0f9eb
Igor França <125696858+IgorSFranca@users.noreply.github.com>
2023-07-02 18:17:55 -0400

    V2
    Escreva um programa que declare um array de inteiros e preencha-o com números digitados pelo usuário. Em seguida, verifique se um número específico digitado pelo usuário está presente no array e imprima uma mensagem correspondente.

--- array7.c
+++ array7.c
@@ -1,22 +1,26 @@
 /*Escreva um programa que declare um array de inteiros e preencha-o com números digitados 
-pelo usuário. Em seguida, ordene os elementos do array em ordem crescente e imprima o 
-array resultante.*/
+pelo usuário. Em seguida, verifique se um número específico digitado pelo usuário está 
+presente no array e imprima uma mensagem correspondente.*/
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main (){
-  int i, num[5];
-  for (i=1; i<=5; i++){
+  int num[4], i, valor, total = 0;
+  for (i=1; i<=4; i++){
     printf("%i numero: ", i);
     scanf("%i", &num[i]);
+      }
+  printf("Informe um numero para ser verificado: ");
+  scanf("%i", &valor);
+  for (i=1; i<=4; i++){
+    if (num[i] == valor)
+      total++;
   }
-  selectionSort (num, 5);
-  
-  printf("O array ordenado: ");
-  for (i=1; i<=5; i++){
-    printf("%i ", num[i]);
-  }
+  if (total == 1)
+    printf ("O valor informado FAZ PARTE do array.\n");
+  else
+    printf("O valor informado NAO FAZ PARTE do array.\n");
   system ("pause");
   return 0;
 }
