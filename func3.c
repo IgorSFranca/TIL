bfa85b4e02c514d387df283504dd95e7d1211241
Igor França <125696858+IgorSFranca@users.noreply.github.com>
2023-07-03 18:45:56 -0400

    v2
    Escreva uma função em C que recebe um número inteiro positivo e retorna o fatorial desse número.

--- func3.c
+++ func3.c
@@ -1,28 +1,22 @@
-/*Faça uma função em C que recebe um array de inteiros e o seu tamanho 
-como parâmetros, e retorna a média dos elementos.*/
+/*Escreva uma função em C que recebe um número inteiro positivo e 
+retorna o fatorial desse número.*/
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void media (num, tam);
-
-int main (){
-  int tam, num, i;
-  printf("Informe quantas notas serao inseridas: ");
-  scanf("%i", &tam);
-  for (i = 0; i<= tam; i++){
-    printf("Informe o %i elemento do array: ", i);
-    scanf("%i", &num);
+void fatorial(int num){
+  int total=1;
+  for (int i=num; i>=1; i--){
+    total = //Descobrir como faz o fatorial
   }
-  media (num, tam);
-  system ("pause");
-  return 0;
+  printf("%i\n", soma);
 }
 
-void media (num, tam){
-  float soma;
-  for (int i=0; i<= tam; i++){
-    soma = soma+num;
-  }
-  printf("A media eh: %.2f\n", (soma/tam));
+int main (){
+  int num;
+  printf("Informe o numero para exibir seu fatorial: ");
+  scanf("%i", &num);
+  fatorial(num);
+  system ("pause");
+  return 0;
 }
