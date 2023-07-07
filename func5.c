70cc97571fe9ec2f60c21b423b4a0c5f8c1ad103
Igor França <125696858+IgorSFranca@users.noreply.github.com>
2023-07-07 16:34:49 -0400

    Gerenciamento de Tarefas

--- func5.c
+++ func5.c
@@ -4,16 +4,11 @@ Por exemplo, se o número for 12345, a função deve retornar 54321.*/
 #include <stdio.h>
 #include <stdlib.h>
 
-void NumInvertido(int num){
-    int i, ninvert[] = num;
-    for (i=sizeof(ninvert); i<=0; i--){
-        printf("%i ", ninvert[i]);
-    }
-}
-
 int main (){
-    int num = {"1325621"};
-    NumInvertido(num);
+    int i, num[] = {1325621};
+    for (i=0; i<= sizeof(num); i++){
+        printf("%i\n", num[i]);
+    }
     system ("pause");
     return 0;
 }
---
+++ gerenc_tarefas.c
@@ -0,0 +1,42 @@
+/*
+Atividade: Sistema de gerenciamento de tarefas
+
+Objetivo: Desenvolver um programa em C que permita ao usuário criar, 
+visualizar e marcar tarefas como concluídas.
+
+Requisitos:
+
+01. O programa deve permitir ao usuário criar uma nova tarefa, 
+informando o título e a descrição da mesma.
+02. O programa deve armazenar as tarefas criadas em um array.
+03. O programa deve permitir ao usuário visualizar todas as tarefas criadas, 
+exibindo o título e o status de cada uma (concluída ou não).
+04. O programa deve permitir ao usuário marcar uma tarefa como concluída, 
+informando o número da tarefa.
+05. O programa deve exibir uma mensagem de erro caso o número da tarefa 
+informado pelo usuário seja inválido.
+06. O programa deve permitir ao usuário sair do sistema quando desejar.
+
+Dicas:
+
+- Utilize uma estrutura (struct) para representar cada tarefa, com 
+campos para o título, descrição e status.
+- Utilize um for loop para percorrer o array de tarefas ao exibi-las e 
+ao verificar o número informado pelo usuário para marcar uma tarefa como concluída.
+- Utilize um switch para implementar as diferentes opções do menu do 
+sistema (criar tarefa, visualizar tarefas, marcar tarefa como concluída, sair do sistema).
+- Lembre-se de utilizar as habilidades mencionadas anteriormente, 
+como variáveis, if/else, switch, for loop, arrays, strings, booleans e 
+funções. Além disso, você também pode considerar a criação de um arquivo 
+para armazenar as tarefas de forma persistente, permitindo que elas sejam 
+recuperadas mesmo após o programa ser encerrado.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+
+  system ("pause");
+  return 0;
+}
