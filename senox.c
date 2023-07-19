eda668ecb47715fe7bef2a2b0ee20f48294e476b
Igor França ∴ <125696858+IgorSFranca@users.noreply.github.com>
2023-07-19 15:57:19 -0400

    correção

--- senox.c
+++ senox.c
@@ -3,17 +3,17 @@
 #include <math.h>
 
 int main (){
-    float soma = 0, fat = 0, g_rad;
+    float soma = 0, fat = 1, g_rad, pi = 3.14;
     int x, i, j;
     printf("Escreva o valor do angulo em graus: ");
     scanf("%i", &x);//recebendo o angulo em graus
-    g_rad = (x*3.14)/180;
+    g_rad = (x*pi)/180;//convertendo o angulo para radianos
     for (i=3; i<=19; i+=2){
         for (j=1; j<=i; j++){
-            fat = fat*j;
+            fat = fat*j;//calculando o fatorial de i
         }
         soma = soma + ((pow(g_rad, i))/fat);
-        fat = 1;
+        fat = 1;//resetando o valor do fatorial para a proxima iteração
     }
     printf("SenX = %.2f\n", g_rad);
     system ("pause");
