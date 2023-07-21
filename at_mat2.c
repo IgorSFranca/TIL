8dc4a648fce6895636c2b64ea7ba4f0e251ca506
Igor França ∴ <125696858+IgorSFranca@users.noreply.github.com>
2023-07-21 08:39:40 -0400

    Opções
    Inseridas as opções de visualizar a matriz e visualizar a posição do menor valor.

--- at_mat2.c
+++ at_mat2.c
@@ -6,15 +6,68 @@ elementos*/
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int main (){
-    int matriz[3][3], i, j;
-    for (i=0; i<3; i++){
-        for (j=0; j<3; j++){
-            printf("Insira um valor para a posicao Linha %i x Coluna %i:", i+1, j+1);
+    int linha = 3, coluna = 3, matriz[linha][coluna], i, j, menor, maior, pos_maior[2] = {0}, pos_menor[2] = {0}, opcao;
+    char resp = 's';
+
+    for (i=0; i<linha; i++){
+        for (j=0; j<coluna; j++){
+            printf("Insira um valor para a posicao Linha %i x Coluna %i: ", i+1, j+1);
             scanf("%i", &matriz[i][j]);
         }
     }
+    for (i=0; i<linha; i++){
+        for (j=0; j<coluna; j++){
+            if (i==0 && j==0){
+                maior = matriz[i][j];
+                menor = matriz[i][j];
+                pos_maior[0] = i;
+                pos_maior[1] = j;
+                pos_menor[0] = i;
+                pos_menor[1] = j;
+            }
+            else if (matriz[i][j] > maior){
+                maior = matriz[i][j];
+                pos_maior[0] = i;
+                pos_maior[1] = j;
+            }
+            else if (matriz[i][j] < menor){
+                menor = matriz[i][j];
+                pos_menor[0] = i;
+                pos_menor[1] = j;
+            }
+        }
+    }
+    do{
+    printf("\nOPCOES DE VISUALIZACAO\n");
+    printf("[1] Visualizar a matriz.\n");
+    printf("[2] Visualizar a posicao do menor valor.\n");
+    printf("[3] Visualizar a posicao do maior valor.\n");
+    printf("\n");
+    printf("Informe sua opcao: ");
+    scanf("%i", &opcao);
+    switch (opcao){
+        case 1: 
+            printf("A matriz inserida foi: ");
+            for (i=0; i<linha; i++){
+                printf("\n");
+                for (j=0; j<coluna; j++){
+                    printf("%i ", matriz[i][j]);
+                }
+
+            }
+            printf("\n");
+            break;
+        case 2:
+            printf("A posicao do menor valor eh a seguinte: \n");
+            printf("Linha: %i e ", pos_menor[0]+1);
+            printf("Coluna: %i.", pos_menor[1]+1);
+            printf("\n");
+            break;
+    }
+    } while (resp == 's');
     system ("pause");
     return 0;
 }
