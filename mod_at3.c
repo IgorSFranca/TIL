3291ab110c2977962da1dbb6d87835593482a04e
Igor França ∴ <125696858+IgorSFranca@users.noreply.github.com>
2023-07-28 14:06:37 -0400

    Comentários
    Inclusão de comentários

--- mod_at3.c
+++ mod_at3.c
@@ -1,16 +1,21 @@
-/*Faça uma solução usando modularidade para automatizar um sistema bancário de um caixa eletrônico. 
-O menu a seguir deve ser exibido ao usuário enquanto a opção informada por ele seja diferente de SAIR:
-1.SALDO 
-2.SAQUE 
-3.PAGAMENTO 
-4.DEPOSITO 
-0.SAIR
-Para cada opção, mostre o saldo no início, e informe ao usuário se a
-operação escolhida foi realizada com sucesso ou não.
-Considere saldo inicial igual $1500
+/* Nome do arquivo: mod_at3.c
+ * Descrição: atividade 3 da aula de modularização do curso de Engenharia da Computação da UFGD.
+        Faça uma solução usando modularidade para automatizar um sistema bancário de um caixa eletrônico. 
+        O menu a seguir deve ser exibido ao usuário enquanto a opção informada por ele seja diferente de SAIR:
+        1.SALDO 
+        2.SAQUE 
+        3.PAGAMENTO 
+        4.DEPOSITO 
+        0.SAIR
+        Para cada opção, mostre o saldo no início, e informe ao usuário se a
+        operação escolhida foi realizada com sucesso ou não.
+        Considere saldo inicial igual $1500
 
-O sistema deve ser consistente: não permitir saque sem ter saldo; não permitir sacar,
-depositar ou pagar valor menor ou igual a zero.*/
+        O sistema deve ser consistente: não permitir saque sem ter saldo; não permitir sacar,
+        depositar ou pagar valor menor ou igual a zero.
+ * Autor: Igor França
+ * Data de criação: 28 de julho de 2023
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
