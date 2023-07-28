698d8212864fa1e60fae7d4a4e66e54f265f0d53
Igor França ∴ <125696858+IgorSFranca@users.noreply.github.com>
2023-07-28 12:44:39 -0400

    Sistema bancário

---
+++ mod_at3.c
@@ -0,0 +1,24 @@
+/*Faça uma solução usando modularidade para automatizar um sistema bancário de um caixa eletrônico. 
+O menu a seguir deve ser exibido ao usuário enquanto a opção informada por ele seja diferente de SAIR:
+1.SALDO 
+2.SAQUE 
+3.PAGAMENTO 
+4.DEPOSITO 
+0.SAIR
+Para cada opção, mostre o saldo no início, e informe ao usuário se a
+operação escolhida foi realizada com sucesso ou não.
+Considere saldo inicial igual $1500
+
+O sistema deve ser consistente: não permitir saque sem ter saldo; não permitir sacar,
+depositar ou pagar valor menor ou igual a zero.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main (){
+
+
+    system ("pause");
+    return 0;
+}
