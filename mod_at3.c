74b5152b8b19999445b586122a44e7faddbebffa
Igor França ∴ <125696858+IgorSFranca@users.noreply.github.com>
2023-07-28 13:02:01 -0400

    Opção de encerramento
    Looping para executar enquanto o usuário não digita a opção de sair

--- mod_at3.c
+++ mod_at3.c
@@ -19,40 +19,44 @@ depositar ou pagar valor menor ou igual a zero.*/
 void cabecalho();
 
 int main (){
-    char resp = 's';
     int opcao;
     float saldo_atual = 1500;
 
-    cabecalho();
-    printf("Informe a opcao: ");
-    scanf("%i", &opcao);
-    switch (opcao){
-        case 1: 
-            printf("Saldo atual: %.2f\n", saldo_atual);
-            break;
-        case 2:
-        case 3:
-        case 4:
-        case 0:
-        default: 
-            printf("Opcao nao encontrada.\n");
-            break;
-    }
-
+    do{
+        cabecalho();
+        printf("Informe a opcao: ");
+        scanf("%i", &opcao);
+        switch (opcao){
+            case 1: //Opção para mostrar o saldo atual
+                printf("Saldo atual: %.2f\n", saldo_atual);
+                break;
+            case 2://Opção para sacar
+            case 3://Opção para pagamento
+            case 4://Opção para depósito
+            case 0://Opção para sair
+                printf("Programa encerrado.\n");
+                break;
+            default: 
+                printf("Opcao nao encontrada.\n");
+                break;
+        }
+    } while (opcao != 0);
     system ("pause");
     return 0;
 }
 
 void cabecalho(){
-    printf("+++++++++++++++++++++++\n");
-    printf("+   SISTEMA BANCARIO  +\n");
-    printf("+++++++++++++++++++++++\n");
-    printf("+       OPCOES        +\n");
-    printf("+ [1] SALDO           +\n");
-    printf("+ [2] SACAR           +\n");
-    printf("+ [3] PAGAMENTO       +\n");
-    printf("+ [4] DEPOSITO        +\n");
-    printf("+ [0] SAIR            +\n");
-    printf("+++++++++++++++++++++++\n\n");
+    printf("++++++++++++++++++++++++++++++\n");
+    printf("+             ~#~            +\n");
+    printf("+       SISTEMA BANCARIO     +\n");
+    printf("+             ~#~            +\n");
+    printf("++++++++++++++++++++++++++++++\n");
+    printf("+            OPCOES          +\n");
+    printf("+ [1] SALDO                  +\n");
+    printf("+ [2] SACAR                  +\n");
+    printf("+ [3] PAGAMENTO              +\n");
+    printf("+ [4] DEPOSITO               +\n");
+    printf("+ [0] SAIR                   +\n");
+    printf("++++++++++++++++++++++++++++++\n\n");
 }
 
