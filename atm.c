169bf71b21e4c15c451b60ed95126d4d6ff57b61
Igor França ∴ <125696858+IgorSFranca@users.noreply.github.com>
2023-08-10 07:36:34 -0400

    Mod função saque
    Modularização da função saque

--- atm.c
+++ atm.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void cabecalho(float saldo);
+void cabecalho(float saldo), visualizar_saldo(float saldo);
+float saque(float valor, float saldo);
 
 int main (){
     float saldo = 0, valor; 
@@ -12,12 +13,8 @@ int main (){
         scanf("%i", &option);
         switch (option){
             case 10:
-                printf("Valor do saque: R$ ");
-                scanf("%f", &valor);
-                saldo = saldo - valor;
-                printf("Saldo atual ......................... R$ %.2f\n\n", saldo);
-                if (saldo < 0)
-                    printf("O Seu saldo esta negativo.\n");
+                saldo = saque(valor, saldo);
+                visualizar_saldo(saldo);
                 break;
             case 33:
                 printf("Valor do deposito: R$ ");
@@ -53,5 +50,27 @@ void cabecalho(float saldo){
     printf("[ 0] Para encerrar                           \n");
     printf("---------------------------------------------\n");
     printf("Saldo inicial ....................... R$ %.2f\n\n", saldo);
+}
 
+float saque(float valor, float saldo){
+    if (saldo <= 0){
+        printf("Saldo insuficiente para saque.\n");
+        return saldo;
+    }
+    do{
+        printf("Valor do saque: R$ ");
+        scanf("%f", &valor);
+        if (valor <= 0)
+            printf("Valor invalido para saque.\n");
+        if (valor > saldo)
+            printf("Saldo insuficiente para saque.\n");
+    } while (valor > saldo||valor <= 0);
+    saldo = saldo - valor;
+    return saldo;
+}
+
+void visualizar_saldo(float saldo){
+    printf("Saldo atual ......................... R$ %.2f\n\n", saldo);
+    if (saldo < 0)
+        printf("O Seu saldo esta negativo.\n");
 }
