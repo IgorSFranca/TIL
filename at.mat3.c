42fbdfb72e34a0962c66cc0e34f3264870c2def5
Igor França ∴ <125696858+IgorSFranca@users.noreply.github.com>
2023-08-10 07:07:14 -0400

    Mod proc matriz_original
    Modularização do procedimento da criação da matriz original

--- at.mat3.c
+++ at.mat3.c
@@ -4,14 +4,12 @@ quadrada de tamanho N. Em seguida, guarde em B a matriz transposta de A*/
 #include <stdio.h>
 #include <stdlib.h>
 
+void criar_matriz(int matriz[3][3]), visualizar_original(int matriz[3][3]), transpor_matriz(int matriz[3][3]), visualizar_transposta(int matriz[3][3]);
+
 int main (){
     int linha = 3, coluna = 3, matriz_orig[linha][coluna], matriz_transp[linha][coluna], i, j;
-    for (i=0; i<linha; i++){//Recebendo os valores para a matriz original
-        for (j=0; j<coluna; j++){
-            printf("Informe o valor para a Linha %i e Coluna %i: ", i+1, j+1);
-            scanf(" %i", &matriz_orig[i][j]);
-        }
-    }
+    criar_matriz(matriz_orig);
+
     for (i=0; i<linha; i++){//Transpondo os valores para a matriz transposta
         for (j=0; j<coluna; j++){
             matriz_transp[i][j] = matriz_orig[j][i];
@@ -35,3 +33,15 @@ int main (){
     system ("pause");
     return 0;
 }
+void criar_matriz(int matriz_orig[3][3]){
+    int i, linha = 3, j, coluna = 3;
+    for (i=0; i<linha; i++){//Recebendo os valores para a matriz original
+        for (j=0; j<coluna; j++){
+            printf("Informe o valor para a Linha %i e Coluna %i: ", i+1, j+1);
+            scanf(" %i", &matriz_orig[i][j]);
+        }
+    }
+} 
+void visualizar_original(int matriz[3][3]){} 
+void transpor_matriz(int matriz[3][3]){} 
+void visualizar_transposta(int matriz[3][3]){}
